53c1adae52707834252b9d6701cfa13ba097c06e
Justin <2573817844@qq.com>
2025-05-02 11:29:16 +0800

    update
    给第二个页面添加了显式跳转回去

--- MyAndroidStudioActivity/app/src/main/java/com/example/activity/SecondActivity.java
+++ MyAndroidStudioActivity/app/src/main/java/com/example/activity/SecondActivity.java
@@ -1,18 +1,39 @@
 package com.example.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.second_layout);
+
+        Button button1 = findViewById(R.id.second_layout_button_1);
+
+
+        //按钮1
+        button1.setOnClickListener(new View.OnClickListener() {
+            // 重写 onClick 方法，当按钮被点击时会调用此方法
+            @Override
+            public void onClick(View v) {
+                //  使用 Toast 类创建一个短暂显示的消息提示，Toast.LENGTH_SHORT 的显示时长约为 2 秒。
+                Toast.makeText(SecondActivity.this, "你使用隐式Intent跳转回到了页面1", Toast.LENGTH_SHORT).show();
+
+                // 使用显式 Intent 跳转回 FirstActivity
+                Intent intent = new Intent(SecondActivity.this, FirstActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // 如果页面1已打开，返回原页面而不是创建新实例
+                startActivity(intent);
+            }
+        });
+
+
     }
+
 }
